9338ebe01837709939703d3668309ae16079d7dc
7281 <373616885>
2019-04-10 14:45:24 +0800

    添加异常处理

--- thread/src/main/java/com/qin/thread/QinThreadFactory.java
+++ thread/src/main/java/com/qin/thread/QinThreadFactory.java
@@ -2,13 +2,17 @@ package com.qin.thread;
 
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.*;
 
 
 /**
  * @Author qinjp
  **/
+@Slf4j
 public final class QinThreadFactory {
 
     private final static int CORE_POOL_SIZE = 10;
@@ -35,6 +39,8 @@ public final class QinThreadFactory {
             // 值必须在 MIN_PRIORITY(1)到 MAX_PRIORITY(10)范围内
             // 要返回一个线程为默认的优先级，指定 NORM_PRIORITY(5)
             .setPriority(Thread.NORM_PRIORITY)
+            // 发送异常后处理
+            .setUncaughtExceptionHandler(new AsyncExceptionHandler())
             .build();
 
     private static class RejectedHandler implements RejectedExecutionHandler {
@@ -43,6 +49,15 @@ public final class QinThreadFactory {
             throw new RejectedExecutionException("task" + r.toString() +
                     " rejected from " + e.toString() +
                     " because of too many tasks");
+        }
+    }
+
+
+    private static class AsyncExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            log.error(t.getName() + " throw Exception occurs in async method", e.getMessage());
         }
     }
 

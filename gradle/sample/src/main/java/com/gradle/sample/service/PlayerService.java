5aa8a0f2349b242ce855dc4cfc6526ddc93da4e7
7281 <373616885>
2019-05-15 18:30:03 +0800

    配置文件修改

--- gradle/sample/src/main/java/com/gradle/sample/service/PlayerService.java
+++ gradle/sample/src/main/java/com/gradle/sample/service/PlayerService.java
@@ -4,6 +4,7 @@ import com.gradle.sample.mybatis.client.PlayerMapper;
 import com.gradle.sample.mybatis.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author qinjp
@@ -14,7 +15,9 @@ public class PlayerService {
     @Autowired
     PlayerMapper playerMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     public void insertPlayer(Player player){
+        playerMapper.deleteByPrimaryKey(player.getUid());
         playerMapper.insert(player);
     }
 }

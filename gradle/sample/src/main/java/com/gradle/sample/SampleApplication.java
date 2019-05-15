4bea34a0cd1ac19a24ea1364db899aea22cc62c8
7281 <373616885>
2019-05-15 18:23:51 +0800

    配置文件修改

--- gradle/sample/src/main/java/com/gradle/sample/SampleApplication.java
+++ gradle/sample/src/main/java/com/gradle/sample/SampleApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.gradle.sample.mybatis.client")
+@MapperScan("com.gradle.*.mybatis.client")
 @SpringBootApplication
 public class SampleApplication {
 

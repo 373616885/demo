564507b6b69fa16062bd76156934cee6ae8b0446
7281 <373616885>
2019-05-16 18:54:45 +0800

    配置文件修改

--- gradle/sample/src/test/java/com/gradle/sample/SampleApplicationTests.java
+++ gradle/sample/src/test/java/com/gradle/sample/SampleApplicationTests.java
@@ -1,16 +1,28 @@
 package com.gradle.sample;
 
+import com.gradle.sample.mybatis.model.Player;
+import com.gradle.sample.service.PlayerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SampleApplicationTests {
 
+    @Resource(name = "playerService")
+    PlayerService playerService ;
+
     @Test
     public void contextLoads() {
+        Player player = new Player();
+        player.setUid("4");
+        player.setName("qin");
+        playerService.insertPlayer(player);
+
     }
 
 }

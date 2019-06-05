97ad1257755097cb2cc02b41a25f8309f8f67a2b
7281 <373616885>
2019-06-05 10:45:53 +0800

    mybatis

--- mybatis/demo/src/main/java/com/qin/mybatis/demo/DemoApplication.java
+++ mybatis/demo/src/main/java/com/qin/mybatis/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.qin.mybatis.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.qin.**.mybatis.client")
 public class DemoApplication {
 
     public static void main(String[] args) {

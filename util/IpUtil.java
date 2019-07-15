1301ca7f2a0bd09a41436104a0163b6dba13b357
三哥 <373616885@qq.com>
2019-07-15 23:37:48 +0800

    Ip工具类

--- util/IpUtil.java
+++ util/IpUtil.java
@@ -13,16 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.nacos.config.server.utils;
+package com.qin.enable.one.web;
+
+import org.apache.catalina.valves.RemoteIpValve;
+import org.apache.commons.lang3.StringUtils;
+import sun.net.util.IPAddressUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Request util
+ * 参考：RemoteIpValve
  *
- * @author Nacos
+ * @author qinjp
  */
 public class IpUtil {
+
+    private static final Pattern INTERNAL_PROXIES = Pattern.compile(
+            "10\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "192\\.168\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "169\\.254\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "127\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "172\\.1[6-9]{1}\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "172\\.2[0-9]{1}\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "172\\.3[0-1]{1}\\.\\d{1,3}\\.\\d{1,3}|" +
+                    "0:0:0:0:0:0:0:1|::1");
 
     private static final String X_REAL_IP = "X-Real-IP";
     private static final String X_FORWARDED_FOR = "X-Forwarded-For";
@@ -30,20 +46,35 @@ public class IpUtil {
 
 
     /**
-     * get real client ip
-     * <p>
-     * first use X-Forwarded-For header    https://zh.wikipedia.org/wiki/X-Forwarded-For
-     * next nginx X-Real-IP
-     * last {@link HttpServletRequest#getRemoteAddr()}
-     *
-     * @param request {@link HttpServletRequest}
-     * @return
+     * 获取真实IP
+     * 首先 判断 getRemoteAddr 是否内网
+     * 接着 用 X-Forwarded-For header  倒序寻找第一个不是内网的  https://zh.wikipedia.org/wiki/X-Forwarded-For
+     * 最后 都没有 就用 nginx X-Real-IP
+     * 都没有就用getRemoteAddr()
      */
     public static String getRemoteIp(HttpServletRequest request) {
+        // 最真实的Ip
+        final String originalRemoteAddr = request.getRemoteAddr();
+
+        // 是否内网--不是内网直接返回
+        if (!INTERNAL_PROXIES.matcher(originalRemoteAddr).matches()) {
+            return originalRemoteAddr;
+        }
+
+        // 内网 --则从 X-Forwarded-For 入手
         String xForwardedFor = request.getHeader(X_FORWARDED_FOR);
         if (!StringUtils.isBlank(xForwardedFor)) {
-            return xForwardedFor.split(X_FORWARDED_FOR_SPLIT_SYMBOL)[0].trim();
+            String[] remoteIp = xForwardedFor.split(X_FORWARDED_FOR_SPLIT_SYMBOL);
+            // 倒序寻找不是内网的IP
+            for (int idx = remoteIp.length - 1; idx >= 0; idx--) {
+                String currentRemoteIp = remoteIp[idx].trim();
+                //  从右边开始第一个不是内网
+                if (!INTERNAL_PROXIES.matcher(currentRemoteIp).matches()) {
+                    return currentRemoteIp;
+                }
+            }
         }
+        // X-Forwarded-For 为空就找 nginx X-Real-IP
         String nginxHeader = request.getHeader(X_REAL_IP);
         return StringUtils.isBlank(nginxHeader) ? request.getRemoteAddr() : nginxHeader;
     }
@@ -77,6 +108,15 @@ public class IpUtil {
         return ipAddress;
     }
 
-   
+
+    public static  boolean isIp(String ipStr){
+        boolean iPv4LiteralAddress = IPAddressUtil.isIPv4LiteralAddress(ipStr);
+        boolean iPv6LiteralAddress = IPAddressUtil.isIPv6LiteralAddress(ipStr);
+        //ip有可能是v4,也有可能是v6,滿足任何一种都是合法的ip
+        if (!(iPv4LiteralAddress||iPv6LiteralAddress)){
+            return false;
+        }
+        return true;
+    }
 
 }

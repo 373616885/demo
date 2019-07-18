8ea04b958716b4254f50e4040e94cacaa2bbdb42
7281 <373616885>
2019-07-18 15:57:32 +0800

    enable

--- enable/enable-start-phone/src/main/java/com/qin/phone/config/PhoneProperties.java
+++ enable/enable-start-phone/src/main/java/com/qin/phone/config/PhoneProperties.java
@@ -1,8 +1,18 @@
 package com.qin.phone.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * @author qinjp
  * @date 2019-07-18
  **/
+@Data
+@ConfigurationProperties(prefix = "enable.qin.phone")
 public class PhoneProperties {
+    private String name;
+    private Integer storage;
+    private String money;
 }
+
+
--- enable/enable-start-phone/src/main/java/com/qin/phone/service/PhoneService.java
+++ enable/enable-start-phone/src/main/java/com/qin/phone/service/PhoneService.java
@@ -1,8 +1,24 @@
 package com.qin.phone.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qin.phone.config.PhoneProperties;
+import lombok.*;
+
 /**
  * @author qinjp
  * @date 2019-07-18
  **/
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class PhoneService {
+
+    private static ObjectMapper ob = new ObjectMapper();
+
+    @NonNull
+    final PhoneProperties phoneProperties;
+
+    @SneakyThrows
+    public String phone() {
+        return ob.writeValueAsString(phoneProperties);
+    }
+
 }

d741722d08a37fda0c3d5c93a011496601c65b8b
7281 <373616885>
2019-08-14 17:43:47 +0800

    redis 批量删除

--- redis/src/main/java/com/redis/mq/controller/SmapleController.java
+++ redis/src/main/java/com/redis/mq/controller/SmapleController.java
@@ -1,7 +1,10 @@
 package com.redis.mq.controller;
 
+import com.redis.mq.util.RedisHelper;
 import com.redis.mq.util.RedisLock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,22 @@ public class SmapleController {
     @Autowired
     private RedisLock redisLock;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/sample/{message}")
     public String sendRedisMessage(@PathVariable String message) {
         redisLock.setIfAbsent20("373616885",message,10000);
+        return "success";
+    }
+
+    @GetMapping("/sample/scon")
+    public String scon() {
+        Cursor<String> cursor = RedisHelper.scan(stringRedisTemplate,"tpl-htmltpl:*",4);
+        while (cursor.hasNext()) {
+            cursor.next();
+        }
+
         return "success";
     }
 }

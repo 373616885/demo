895de7786e5bcd294a022fe83dad7d584a4259e3
7281 <373616885>
2019-08-15 09:39:33 +0800

    redis 批量删除

--- redis/src/main/java/com/redis/mq/controller/SmapleController.java
+++ redis/src/main/java/com/redis/mq/controller/SmapleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.soap.Addressing;
+import java.time.Instant;
 
 /**
  * @author qinjp

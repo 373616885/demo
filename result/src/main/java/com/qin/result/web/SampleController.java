c858861372cf3bd0e54091eaa48ecff7bb1eca57
三哥 <373616885@qq.com>
2019-08-20 23:19:19 +0800

    spring mvc 请求参数名称不对应处理

--- result/src/main/java/com/qin/result/web/SampleController.java
+++ result/src/main/java/com/qin/result/web/SampleController.java
@@ -7,6 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 
 @RestController
 @AllArgsConstructor
@@ -17,7 +21,21 @@ public class SampleController {
     private final Third third;
 
     @RequestMapping("/documentServerProperties")
-    public Object getObjectProperties () {
+    public Object getObjectProperties() {
+        return documentServerProperties.toString() + " " + third.toString();
+    }
+
+
+    @RequestMapping("/cookie/abc/qin")
+    public Object cookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("acctoken", UUID.randomUUID().toString());
+        // http://localhost:8081/cookie/abc/qin
+        // 只有 /cookie/abc/ 路径下才能获取到这个cookie
+//        cookie.setDomain("localhost");
+//        cookie.setHttpOnly(true);
+//        cookie.setPath("/");
+//        cookie.setMaxAge(30);
+        response.addCookie(cookie);
         return documentServerProperties.toString() + " " + third.toString();
     }
 

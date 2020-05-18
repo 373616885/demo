0da0e2364accfa5c077742b8dddf0675f920ab4c
三哥 <373616885@qq.com>
2020-05-18 23:48:39 +0800

    List

---
+++ 基础/project/base/src/main/java/com/qin/base/LinkedListBase.java
@@ -0,0 +1,16 @@
+package com.qin.base;
+
+import java.util.LinkedList;
+
+public class LinkedListBase {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        System.out.println(linkedList);
+
+
+    }
+
+}
--- 基础/project/base/src/main/java/com/qin/base/MapBase.java
+++ 基础/project/base/src/main/java/com/qin/base/MapBase.java
@@ -2,18 +2,56 @@ package com.qin.base;
 
 import java.util.*;
 
-public class MapBase {
+public class MapBase<E> {
 
-    public static void main(String[] args) throws InterruptedException {
-        ArrayList<Integer> list = new ArrayList<>(4);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        System.out.println(list);
+    transient int size = 0;
+
+    /**
+     * Pointer to first node.
+     * Invariant: (first == null && last == null) ||
+     *            (first.prev == null && first.item != null)
+     */
+    transient Node<E> first;
+
+    /**
+     * Pointer to last node.
+     * Invariant: (first == null && last == null) ||
+     *            (last.next == null && last.item != null)
+     */
+    transient Node<E> last;
+
+
+    private void linkFirst(E e) {
+        // 原第一个
+        final Node<E> f = first;
+
+        final Node<E> newNode = new Node<>(null, e, f);
+        // 新的作为第一个
+        first = newNode;
+
+        if (f == null) {
+            // 第一次 第一个 为空 last 就是本事 形成一个闭环
+            last = newNode;
+        } else {
+            // 否则就是之前的first
+            f.prev = newNode;
+        }
+        size++;
     }
 
+
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 
 }
 

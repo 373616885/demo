200a404025ff1b0e08429b5b37e2fd63cb2af76a
qinjiepeng <373616885@qq.com>
2020-09-12 18:57:37 +0800

    gz

---
+++ gz-airport/cache/src/main/java/com/zzsim/gz/airport/cache/base/Constant.java
@@ -0,0 +1,17 @@
+package com.zzsim.gz.airport.cache.base;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author qinjp
+ * @date 2020/9/12
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constant {
+
+    public static final String  KEY_PREFIX ="gz:airport";
+
+
+
+}
---
+++ gz-airport/cache/src/main/java/com/zzsim/gz/airport/cache/config/CaffeineConfig.java
@@ -0,0 +1,42 @@
+package com.zzsim.gz.airport.cache.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
+import com.google.common.collect.ImmutableList;
+import com.zzsim.gz.airport.cache.base.Constant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ *  本地缓存配置类
+ *
+ * @author qinjp
+ * @date 2020/9/12
+ */
+@Configuration
+public class CaffeineConfig {
+
+    @Value("${spring.cache.caffeine.spec}")
+    private String caffeineSpec = "initialCapacity=10,maximumSize=500,expireAfterWrite=1800s";
+
+
+    @Bean(name = "caffeineCacheManager")
+    public CacheManager cacheManagerWithCaffeineFromSpec(){
+        CaffeineSpec spec = CaffeineSpec.parse(caffeineSpec);
+        Caffeine<Object, Object> caffeine = Caffeine.from(spec);
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        cacheManager.setCacheNames(getNames());
+        return cacheManager;
+    }
+
+    private static List<String> getNames(){
+        return ImmutableList.of(Constant.KEY_PREFIX);
+    }
+
+}

0ed1ba87da29662343588f912e71c9071d89e479
qinjp <373616885@qq.com>
2020-09-13 20:36:53 +0800

    gz

--- gz-airport/cache/src/main/java/com/zzsim/gz/airport/cache/base/CacheConstant.java
+++ gz-airport/cache/src/main/java/com/zzsim/gz/airport/cache/base/CacheConstant.java
@@ -1,17 +1,11 @@
 package com.zzsim.gz.airport.cache.base;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
 /**
  * @author qinjp
  * @date 2020/9/12
  */
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class CacheConstant {
+public interface CacheConstant {
 
-    public static final String  KEY_PREFIX ="gz:airport";
-
-
+    String KEY_PREFIX = "gz:airport";
 
 }

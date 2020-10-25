7c61ea967f59c8e2319c1baca69aa61e975e6d17
qinjp <373616885@qq.com>
2020-10-25 23:29:54 +0800

    knife4j

--- doc/knife4j/src/main/java/com/qin/knife4j/Knife4jApplication.java
+++ doc/knife4j/src/main/java/com/qin/knife4j/Knife4jApplication.java
@@ -2,12 +2,15 @@ package com.qin.knife4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @SpringBootApplication
 public class Knife4jApplication {
 
+	/**
+	 * knife4j文档地址
+	 * http://localhost:8080/doc.html
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Knife4jApplication.class, args);
 	}
--- doc/knife4j/src/main/java/com/qin/knife4j/config/DocConfig.java
+++ doc/knife4j/src/main/java/com/qin/knife4j/config/DocConfig.java
@@ -2,6 +2,7 @@ package com.qin.knife4j.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,7 +10,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Profile("dev")
+@EnableSwagger2
 @Configuration
 public class DocConfig {
 
